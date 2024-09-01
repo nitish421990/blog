e5f83a66cbb7ca904b92c17229b0906ebcf80a42
nitish421990 <41812918+nitish421990@users.noreply.github.com>
2024-09-01 20:24:16 +0530

    laravel blog  website

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+
